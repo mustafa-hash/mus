57bd71777cebe22962cefb0095e69336a2d5b204
mustafa-hash <73363584+mustafa-hash@users.noreply.github.com>
2021-03-29 09:38:41 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+    <?php
+
+require_once './includes/header.php';
+
+?>
+
+
+<section class="container" style="min-height: 100%">
+      <h3>county revenue collection</h3>
+    </section>
+
+
+<?php
+ require_once './includes/footer.php';
+
+ ?>
+   
